e5ee5d81e0fb1d4ee18b42b21eac6a8be14cb281
Egge <egge@my2sats.space>
2025-08-28 17:10:17 +0200

    feat(core): reexport decoding utils

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,3 +3,4 @@ export * from './repositories/index.ts';
 export * from './models/index.ts';
 export type { CoreProof } from './types.ts';
 export { type Logger, ConsoleLogger } from './logging/index.ts';
+export { getEncodedToken, getDecodedToken } from '@cashu/cashu-ts';
