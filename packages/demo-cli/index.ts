3134fbd24d314fba5006d557ac08144f2f7d87be
Egge <egge@my2sats.space>
2025-09-08 16:14:07 +0200

    feat(cli): updated to latest sdk api

--- packages/demo-cli/index.ts
+++ packages/demo-cli/index.ts
@@ -3,17 +3,42 @@ import { ConsoleLogger, Manager } from 'coco-cashu-core';
 import { SqliteRepositories } from 'coco-cashu-sqlite3';
 import { Database } from 'sqlite3';
 import { getEncodedToken } from '@cashu/cashu-ts';
+import * as bip39 from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english';
 
 const db = new Database('./test.db');
+
+db.exec(`CREATE  TABLE IF NOT EXISTS config (
+  key TEXT PRIMARY KEY,
+  value TEXT NOT NULL
+)`);
+
+let cachedKey = await new Promise<string | null>((resolve, reject) => {
+  db.get<{ value: string }>('SELECT value FROM config WHERE key = "mnemonic"', (err, row) => {
+    if (err) reject(err);
+    if (!row) {
+      resolve(null);
+    } else {
+      resolve(row.value);
+    }
+  });
+});
+if (!cachedKey) {
+  const newKey = bip39.generateMnemonic(wordlist);
+  db.run('INSERT INTO config (key, value) VALUES (?, ?)', ['mnemonic', newKey]);
+  cachedKey = newKey;
+}
+const seedGetter = async () => bip39.mnemonicToSeedSync(cachedKey);
+
 const repo = new SqliteRepositories({ database: db });
 await repo.init();
-const manager = new Manager(repo, new ConsoleLogger());
+const manager = new Manager(repo, seedGetter, new ConsoleLogger(undefined, { level: 'debug' }));
 
 program
   .command('balance')
   .description('get wallet balance')
   .action(async function (env, options) {
-    const balance = await manager.getBalances();
+    const balance = await manager.wallet.getBalances();
     console.log(balance);
   });
 
@@ -23,7 +48,7 @@ program
   .argument('<mintUrl>', 'the mint url to add')
   .action(async function (mintUrl) {
     try {
-      await manager.addMint(mintUrl);
+      await manager.mint.addMint(mintUrl);
     } catch (e) {
       console.error(e);
     }
@@ -36,7 +61,7 @@ program
   .argument('quoteId')
   .action(async function (mintUrl, quoteId) {
     try {
-      await manager.redeemMintQuote(mintUrl, quoteId);
+      await manager.quotes.redeemMintQuote(mintUrl, quoteId);
       console.log('Redeemed quote!');
     } catch (e) {
       console.error(e);
@@ -50,7 +75,7 @@ program
   .argument('<amount>')
   .action(async function (mintUrl, amount) {
     try {
-      const quote = await manager.createMintQuote(mintUrl, Number(amount));
+      const quote = await manager.quotes.createMintQuote(mintUrl, Number(amount));
       console.log('Quote created: ', quote.quote);
       console.log('Please pay this invoice: ', quote.request);
     } catch (e) {
@@ -65,7 +90,7 @@ program
   .argument('<amount>')
   .action(async function (mintUrl, amount) {
     try {
-      const toSend = await manager.send(mintUrl, Number(amount));
+      const toSend = await manager.wallet.send(mintUrl, Number(amount));
       console.log(getEncodedToken(toSend));
     } catch (e) {
       console.error(e);
@@ -77,7 +102,7 @@ program
   .argument('<token>', 'a cashu token')
   .action(async (token) => {
     try {
-      await manager.receive(token);
+      await manager.wallet.receive(token);
     } catch (e) {
       console.error(e);
     }
