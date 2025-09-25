52b24cfe20173f7cb360acd8c6cb9d441159d1c4
Egge <egge@my2sats.space>
2025-09-25 15:33:52 +0200

    docs(docs): init

---
+++ packages/docs/.vitepress/config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Coco Cashu Docs',
+  description: 'Cashu out of the box',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
+});
